6a30db506f02b146c114394b71594a7cf38f753e
MrNullus <gustavojs417@gmail.com>
2024-09-21 16:31:59 -0300

    feat: styles header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,19 @@
+import stylesGlobals from "../../app/resources/styles/globals.module.scss";
 import styles from "./style.module.scss";
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <div className="logo-container">
         <img className="logo" src="/images/logo.svg" alt="FocalPoint"/>
       </div>
       <div className={ "message-welcome" }>
-        <h1>
+        <h1 className={stylesGlobals.title}>
           Bem-vindo de volta, Marcus
         </h1>
       </div>
       <div className={ "message-date" }>
-        <h2>
+        <h2 className={stylesGlobals.subtitle}>
           Segunda, 01 de dezembro de 2025
         </h2>
       </div>
