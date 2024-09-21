341255796a2c08d3dcb14361557b0a2b60d9e315
MrNullus <gustavojs417@gmail.com>
2024-09-20 23:49:22 -0300

    feat: estrutura básica da page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,35 @@
 import styles from "./page.module.scss";
+import Header from "@/components/Header";
+import AreaTasksUncompleted from "@/components/AreaTasksUncompleted";
+import AreaTasksCompleted from "@/components/AreaTasksCompleted";
+import ModalDeleteTask from "@/components/ModalDeleteTask";
+import ModalAddTask from "@/components/ModalAddTask";
 
 export default function Home() {
-    return (
-        <>
-            adsad
-        </>
-    );
+  return (
+    <div className={ styles.page }>
+
+      <Header/>
+
+      <main className={ "app-container" }>
+
+        <div className="wrapper">
+          <section className="tasks-container">
+
+            <AreaTasksUncompleted/>
+
+            <AreaTasksCompleted/>
+          </section>
+
+          <button className="btn-add-task">
+            Adicionar nova tarefa
+          </button>
+        </div>
+
+      </main>
+
+      <ModalAddTask />
+      <ModalDeleteTask />
+    </div>
+  );
 }
